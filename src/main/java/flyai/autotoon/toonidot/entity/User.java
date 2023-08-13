f1306864c176c4afe2f3ff9b0a0b820832df4fd7
kang <kang6@ewhain.net>
2023-08-13 16:47:06 +0900

    Add User Entity

---
+++ src/main/java/flyai/autotoon/toonidot/entity/User.java
@@ -0,0 +1,29 @@
+package flyai.autotoon.toonidot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String email;
+
+}
