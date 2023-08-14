42654b089dc062b62a208b78311b046de88ef63c
kang <kang6@ewhain.net>
2023-08-14 09:05:01 +0900

    Create basic files

---
+++ src/main/java/flyai/autotoon/toonidot/repository/InfoRepository.java
@@ -0,0 +1,8 @@
+package flyai.autotoon.toonidot.repository;
+
+
+import flyai.autotoon.toonidot.entity.Info;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InfoRepository extends JpaRepository<Info, Long> {
+}
