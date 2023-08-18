bbcc008095a7617db8f6fca2860b8d1ded397c0e
Simba <be8shyn+git@gmail.com>
2023-08-18 10:25:11 +0900

    [ADD] Add Cartoon Controller

--- src/main/java/flyai/autotoon/toonidot/controller/CartoonApiController.java
+++ src/main/java/flyai/autotoon/toonidot/controller/CartoonApiController.java
@@ -1,9 +1,27 @@
 package flyai.autotoon.toonidot.controller;
 
-import lombok.RequiredArgsConstructor;
+import flyai.autotoon.toonidot.dto.CartoonDto;
+import flyai.autotoon.toonidot.service.CartoonService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
+import java.util.List;
+
+
 @RestController
+@RequestMapping("/api/cartoons")
 public class CartoonApiController {
+    private final CartoonService cartoonService;
+
+    public CartoonApiController(CartoonService cartoonService) {
+        this.cartoonService = cartoonService;
+    }
+
+    @GetMapping("/{userId}")
+    public List<CartoonDto> getUserCartoonsAndStories(@PathVariable Long userId){
+        return cartoonService.getAllCartoonsAndStory(userId);
+    }
 }
