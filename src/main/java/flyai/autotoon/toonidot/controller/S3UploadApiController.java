1934e36f8cb2f6c211bba8bc2acbeb95203a6caf
se0hyun <kimlisa00@seoultech.ac.kr>
2023-08-18 18:11:05 +0900

    [MOD] Modify S3 Upload Controller

--- src/main/java/flyai/autotoon/toonidot/controller/S3UploadApiController.java
+++ src/main/java/flyai/autotoon/toonidot/controller/S3UploadApiController.java
@@ -1,6 +1,9 @@
 package flyai.autotoon.toonidot.controller;
 
+import flyai.autotoon.toonidot.entity.Info;
+import flyai.autotoon.toonidot.repository.InfoRepository;
 import flyai.autotoon.toonidot.service.S3UploadService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,19 +14,20 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class S3UploadApiController {
     private final S3UploadService s3UploadService;
-
-    @Autowired
-    public S3UploadApiController(S3UploadService s3UploadService) {
-        this.s3UploadService = s3UploadService;
-    }
+    private final InfoRepository infoRepository;
 
     @PostMapping("/upload-image")
     public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile imageFile,
-                                              @RequestParam("fileName") String fileName) {
+                                              @RequestParam("fileName") String fileName,
+                                              @RequestParam("infoId") Long infoId) {
+
         try {
-            String imageUrl = s3UploadService.uploadImage(imageFile, fileName);
+            Info info = infoRepository.findById(infoId)
+                    .orElseThrow(()->new RuntimeException("Webtoon Info Not Found"));
+            String imageUrl = s3UploadService.uploadImage(imageFile, fileName, info);
             return ResponseEntity.ok("Uploaded image URL: " + imageUrl);
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Image upload failed");
