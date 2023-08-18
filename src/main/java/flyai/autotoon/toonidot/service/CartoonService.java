e46e9b6b14f1436ce5a4bc5adec6bd7134d29e72
Simba <be8shyn+git@gmail.com>
2023-08-18 10:32:24 +0900

    [ADD] Add Cartoon Service

--- src/main/java/flyai/autotoon/toonidot/service/CartoonService.java
+++ src/main/java/flyai/autotoon/toonidot/service/CartoonService.java
@@ -1,9 +1,38 @@
 package flyai.autotoon.toonidot.service;
 
+import flyai.autotoon.toonidot.dto.CartoonDto;
+import flyai.autotoon.toonidot.entity.Cartoon;
+import flyai.autotoon.toonidot.entity.Info;
+import flyai.autotoon.toonidot.repository.CartoonRepository;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.Hibernate;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 @Service
+@RequiredArgsConstructor
 public class CartoonService {
+    private final CartoonRepository cartoonRepository;
+
+    @Transactional
+    public List<CartoonDto> getAllCartoonsAndStory(Long userID){
+        List<Cartoon> cartoons = cartoonRepository.findByInfoUserUserId(userID);
+        return cartoons.stream()
+                .map(this::mapToDTO)
+                .collect(Collectors.toList());
+    }
+
+    private CartoonDto mapToDTO(Cartoon cartoon){
+        Info info = cartoon.getInfo();
+        Hibernate.initialize(info);
+        return CartoonDto.builder()
+                .infoId(cartoon.getCartoonId())
+                .cartoonURL(cartoon.getCartoonURL())
+                .storyContent(cartoon.getInfo().getStory().getStoryContent())
+                .build();
+    }
 }
