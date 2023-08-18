d7df2a2d13a1eb517df24287a994c1c4b6fc0943
se0hyun <kimlisa00@seoultech.ac.kr>
2023-08-18 18:14:50 +0900

    [MOD] modify S3 Upload Service

--- src/main/java/flyai/autotoon/toonidot/service/S3UploadService.java
+++ src/main/java/flyai/autotoon/toonidot/service/S3UploadService.java
@@ -2,6 +2,9 @@ package flyai.autotoon.toonidot.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import flyai.autotoon.toonidot.entity.Cartoon;
+import flyai.autotoon.toonidot.entity.Info;
+import flyai.autotoon.toonidot.repository.CartoonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,11 +16,13 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class S3UploadService {
     private final AmazonS3 amazonS3;
+    private final CartoonRepository cartoonRepository;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public String uploadImage(MultipartFile imageFile, String fileName) throws IOException {
+
+    public String uploadImage(MultipartFile imageFile, String fileName, Info info) throws IOException {
         String originalFilename = fileName + ".jpg"; // 이미지 확장자에 맞게 설정
 
         // 이미지 파일을 S3에 업로드
@@ -29,6 +34,12 @@ public class S3UploadService {
         // 업로드된 이미지의 URL 반환
         String imageUrl = amazonS3.getUrl(bucket, originalFilename).toString();
         System.out.println("Uploaded file URL:" + imageUrl);
+
+
+        Cartoon cartoon = new Cartoon();
+        cartoon.setInfo(info);
+        cartoon.setCartoonURL(imageUrl);
+        cartoonRepository.save(cartoon);
         return imageUrl;
     }
 }
