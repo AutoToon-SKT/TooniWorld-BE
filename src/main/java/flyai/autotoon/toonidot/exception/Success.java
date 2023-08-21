ea9f8a5bfe28afc064403d4fd9cfb6df0540f61a
se0hyun <kimlisa00@seoultech.ac.kr>
2023-08-21 09:56:46 +0900

    [ADD] Save Cartoon Message

--- src/main/java/flyai/autotoon/toonidot/exception/Success.java
+++ src/main/java/flyai/autotoon/toonidot/exception/Success.java
@@ -12,7 +12,8 @@ public enum Success {
     SAVE_INFO_SUCCESS(HttpStatus.OK, "카툰 정보가 성공적으로 저장되었습니다."),
     UPDATE_INFO_SUCCESS(HttpStatus.OK, "카툰 정보가 성공적으로 수정되었습니다."),
     SAVE_STORY_SUCCESS(HttpStatus.OK, "당신의 이야기가 성공적으로 저장되었습니다."),
-    UPDATE_STORY_SUCCESS(HttpStatus.OK, "당신의가 성공적으로 수정되었습니다.");
+    UPDATE_STORY_SUCCESS(HttpStatus.OK, "당신의 이야기가 성공적으로 수정되었습니다."),
+    SAVE_CARTOON_SUCCESS(HttpStatus.OK, "카툰이 성공적으로 저장되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
