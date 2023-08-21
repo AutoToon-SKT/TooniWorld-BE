20209fb6d64c8f8958cf8248e7fc3a2afee13862
se0hyun <kimlisa00@seoultech.ac.kr>
2023-08-21 16:28:37 +0900

    [Mod] Success Message
    Pull Request Conflict

--- src/main/java/flyai/autotoon/toonidot/exception/Success.java
+++ src/main/java/flyai/autotoon/toonidot/exception/Success.java
@@ -14,7 +14,6 @@ public enum Success {
     SAVE_STORY_SUCCESS(HttpStatus.OK, "당신의 이야기가 성공적으로 저장되었습니다."),
     UPDATE_STORY_SUCCESS(HttpStatus.OK, "당신의 이야기가 성공적으로 수정되었습니다."),
     SEND_ALL_STORY_SUCCESS(HttpStatus.OK, "당신의 만화 이야기가 모두 성공적으로 전송되었습니다."),
-    SAVE_CARTOON_SUCCESS(HttpStatus.OK, "카툰이 성공적으로 저장되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
